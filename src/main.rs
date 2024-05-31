485dde562596cae9e8e0d3f504840b0d21f7bc7b
NoFrequency <60072696+NoFrequency@users.noreply.github.com>
2024-05-31 22:38:12 +0200

    add miner construction

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use patination::add;
+use patination::database::Database;
+use patination::miner::Miner;
 
 fn main() {
-    println!("Hello, patination {}!", add(1046, 291));
+    let sample_synthetic_data: &[String] = &["abcdefghijklmnopqrstuvxyz".to_string()];
+    let database = Database::from(sample_synthetic_data);
+    let miner = Miner::with_database(database);
+
+    println!("Hello, patination {}!", miner.mdl_size());
 }
